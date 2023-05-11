2f518451b884b510c7815d37680068c47074b1ad
Teemu Lähteinen <teemu6@gmail.com>
2023-05-11 22:07:54 +0300

    Changed default linter for python

--- digit_recognition_microservice/digit_recognition_microservice.py
+++ digit_recognition_microservice/digit_recognition_microservice.py
@@ -4,68 +4,73 @@ import keras.models as models
 from flask import Flask, request, jsonify
 
 # load the model
-model = models.load_model('mymodel')
+model = models.load_model("mymodel")
 
 # initialize the Flask app
 app = Flask(__name__)
 
+
 def preprocess_image(img):
     # resize the input image to a fixed size of 28x28 pixels
-    img = cv2.resize(img, (28,28))
-    
+    img = cv2.resize(img, (28, 28))
+
     # invert the image colors
     img = cv2.bitwise_not(img)
-    
+
     # normalize the pixel values to be between -0.5 and 0.5
     img = (img / 255) - 0.5
-    
+
     # add an extra dimension to the image tensor to represent a batch of size 1
     img_tensor = np.expand_dims(img, axis=0)
-    
+
     # return the preprocessed image tensor
     return img_tensor
 
+
 def segment_image(img):
     # Threshold the image
-    _, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    
+    _, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
     # Find contours
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
+
     # Sort contours from left to right
     contours = sorted(contours, key=lambda x: cv2.boundingRect(x)[0])
-    
+
     # Extract each digit and predict class id
     predictions = []
-    
+
     for cnt in contours:
         # Get the bounding rectangle coordinates for the contour
-        x,y,w,h = cv2.boundingRect(cnt)
-        
+        x, y, w, h = cv2.boundingRect(cnt)
+
         # Crop the digit from the original image using the bounding rectangle coordinates
-        digit = img[y:y+h, x:x+w]
-        
+        digit = img[y : y + h, x : x + w]
+
         # Add white padding around the digit
-        digit = cv2.copyMakeBorder(digit, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[255, 255, 255])
-        
+        digit = cv2.copyMakeBorder(
+            digit, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value=[255, 255, 255]
+        )
+
         # Preprocess the digit image for prediction
         digit = preprocess_image(digit)
-        
+
         # Make a prediction for the digit image using the pre-trained model
         prediction = model.predict(digit)
-        
+
         # Get the predicted class ID for the digit image
         class_id = np.argmax(prediction)
-        
+
         # Append the predicted class ID to the list of predictions
         predictions.append(int(class_id))
-        
+
     return predictions
 
-@app.route('/recognizeimage', methods=['POST'])
+
+@app.route("/recognizeimage", methods=["POST"])
 def predict_number():
     # get the image from the POST request
-    img_file = request.files['image']
+    img_file = request.files["image"]
 
     # convert file object to bytes
     img_bytes = img_file.read()
@@ -77,11 +82,12 @@ def predict_number():
     img = cv2.imdecode(img_array, cv2.IMREAD_GRAYSCALE)
 
     # segment image into individual digits
-    predictions = segment_image(img)
+    digits = segment_image(img)
 
     # make a prediction and return the result as JSON
-    response = {'class_ids': predictions}
+    response = {"digits": digits}
     return jsonify(response)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=5005)
